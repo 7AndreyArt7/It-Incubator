2820b3d63eb7b0ac1febf2a61164ca338c8e549d
7AndreyArt7 <7andron7art@gmail.com>
2025-01-11 23:26:37 +0300

    new commit

--- CodeWars.jsx
+++ CodeWars.jsx
@@ -4,3 +4,55 @@ function add(n) {
     }
   };
   
+
+  const array = [true, true, false];
+const op = 'AND';
+function logicalCalc(array, op){
+   var result = array[0];
+   for(var i = 1; i < array.length; i++){
+      if(op == "AND"){
+         result = result && array[i];
+      }
+      if(op == "OR"){
+         result = result || array[i];
+      }
+      if(op == "XOR"){
+         result = result != array[i];
+      }
+   }
+   return result;
+}
+console.log(logicalCalc(array, op));
+
+
+
+
+let arr1 = [1, 2, 2, 2, 3];
+let arr2 = [2];
+
+function arrayDiff(a, b) {
+    let result = []
+    for (let i = 0; i < a.length; i++) {
+        if (b.indexOf(a[i]) === -1) {
+            result.push(a[i])
+        }
+    }
+    return result;
+}
+console.log(arrayDiff(arr1, arr2))
+
+
+
+function createPhoneNumber(numbers) {
+  if (numbers.length !== 10) {
+      return "Invalid input.";
+  }
+  let firstPart = numbers.slice(0, 3).join('');
+  let secondPart = numbers.slice(3, 6).join('');
+  let thirdPart = numbers.slice(6).join('');
+
+  const phoneNumber = `(${firstPart}) ${secondPart}-${thirdPart}`;
+
+  return phoneNumber;
+}
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
